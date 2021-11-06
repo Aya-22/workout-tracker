c73fd055b8eae848cddafd52fa86c5056a3f1965
Jennifer Ayala <j.ayala0695@gmail.com>
2021-11-05 22:43:19 -0700

    added workout model

---
+++ models/workout.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercise: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise name is required",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Exercise name is required",
+      },
+      duration: {
+        type: Number,
+        
+      },
+      weight: {
+        type: Number,
+        
+      },
+      reps: {
+        type: Number,
+        
+      },
+      set: {
+        type: Number,
+        
+      },
+      distance: {
+        type: Number,
+        
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
