f832ae82973309ba5792cc767df631d213badc9e
Jennifer Ayala <j.ayala0695@gmail.com>
2021-11-15 22:30:05 -0800

    updated get range on apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,11 @@ const Workout = require("../models/Workout.js");
 const mongoose = require('mongoose');
 
 // this function the total duration of each workout from the past seven workouts on the stats page.  $addFields
-router.get("/api/workout/duration", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
   Workout.aggregate([
     {
       $addFields: {
-        totalDuration: { $sum: exercise.duration }
+        totalDuration: { $sum: "exercise.duration" }
       }
     }])
     .limit(7)
@@ -20,7 +20,7 @@ router.get("/api/workout/duration", (req, res) => {
   
 });
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/workouts/", ({ body }, res) => {
     Workout.create(body)
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -31,7 +31,7 @@ router.post("/api/workout", ({ body }, res) => {
   });
 
 
-router.put("/api/workout/:id", ({ body }, res) => {
+router.put("/api/workouts/:id", ({ body }, res) => {
   Workout.findByIdAndUpdate(
   { _id: body.id },
   { exercise: body.exercise},
@@ -45,7 +45,7 @@ router.put("/api/workout/:id", ({ body }, res) => {
 });
 
 // 
-router.get("/api/workout/weight", (req, res) => {
+router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .sort({ date: -1 })
     .then(dbWorkout => {
