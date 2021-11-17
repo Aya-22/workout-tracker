98b81e8da9cfc2d2b7dde9191770ae6f05429622
Jennifer Ayala <j.ayala0695@gmail.com>
2021-11-16 18:06:22 -0800

    updated const Workoukout on apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout.js");
+const Workout = require("../models/Workout");
 
 
 // this function the total duration of each workout from the past seven workouts on the stats page.  $addFields
